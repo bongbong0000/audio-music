2013248d87cbc660161d035b6e4bd8806e05712a
bongbong0000 <63562028+bongbong0000@users.noreply.github.com>
2020-09-03 21:54:21 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+import GacLaiAuLo from"./GacLaiAuLo-DaLABMiuLe-6360815.mp3";
+import SauTatCa from"./Sau Tất Cả_ERIK ST.319_-1075455731.mp3";
+import PayPhone from"./Payphone_Maroon 5, Wiz Khalifa_-1074177246.mp3";
+import PhutBanDau from"./Phut-Ban-Dau-Vu.mp3";
+import TinhYeuBanBiu from"./Tinh Yeu Ban Biu - Rap Viet 2020_ - TLin.mp3";
+
+const audios =[
+
+{
+	src:GacLaiAuLo,
+	title:" Gác Lại Âu Lo",
+	artist:"Miu-le Dalab",
+},
+{
+	src:SauTatCa,
+	title:" Sau Tất Cả",
+	artist:"Erik",
+},
+{
+	src:PayPhone,
+	title:" Pay_Phone",
+	artist:"maroon",
+},
+{
+	src:PhutBanDau,
+	title:" Phút Ban Đầu",
+	artist:"Vũ",
+},
+{
+	src:TinhYeuBanBiu,
+	title:"Tình Yêu Bận Bịu",
+	artist:"TLinh",
+},
+];
+  
+
+export default audios;
